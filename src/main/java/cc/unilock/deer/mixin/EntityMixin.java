347d197aba91d093be59b12ddf507e8b7bbb9b84
unilock <unilock@fennet.rentals>
2024-11-25 18:47:26 -0500

    fix the lightning strike recipe
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/deer/mixin/EntityMixin.java
+++ src/main/java/cc/unilock/deer/mixin/EntityMixin.java
@@ -3,6 +3,8 @@ package cc.unilock.deer.mixin;
 import cc.unilock.deer.init.DeerItems;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,11 +13,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Entity.class)
 public class EntityMixin {
 	@Inject(method = "onStruckByLightning(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/LightningEntity;)V", at = @At("HEAD"), cancellable = true)
-	private void onStruckByLightning(CallbackInfo ci) {
+	private void onStruckByLightning(ServerWorld world, LightningEntity lightning, CallbackInfo ci) {
 		if (ItemEntity.class.isAssignableFrom(getClass())) {
-			ItemEntity itemEntity = (ItemEntity) (Object) this;
-			if (itemEntity.getStack().isOf(DeerItems.RAW_DEER)) {
-				itemEntity.setStack(DeerItems.PROCESSED_DEER.getDefaultStack());
+			ItemEntity item = (ItemEntity) (Object) this;
+			if (item.getStack().isOf(DeerItems.RAW_DEER)) {
+				ItemEntity processed = new ItemEntity(world, item.getX(), item.getY(), item.getZ(), DeerItems.PROCESSED_DEER.getDefaultStack());
+				item.discard();
+				world.spawnEntity(processed);
+				ci.cancel();
+			}
+			if (item.getStack().isOf(DeerItems.PROCESSED_DEER)) {
 				ci.cancel();
 			}
 		}
