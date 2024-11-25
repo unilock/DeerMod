2f1c18f22451e85d6002e73502ef999676e5d822
unilock <unilock@fennet.rentals>
2024-11-25 16:58:39 -0500

    raw deer recipe fix
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/deer/mixin/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/cc/unilock/deer/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -3,19 +3,21 @@ package cc.unilock.deer.mixin;
 import cc.unilock.deer.init.DeerItems;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.RecipeEntry;
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.util.collection.DefaultedList;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
 public class AbstractFurnaceBlockEntityMixin {
 	@WrapOperation(method = "craftRecipe", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;decrement(I)V"))
-	private static void craftRecipe$decrement(ItemStack instance, int amount, Operation<Void> original, @Local LocalRef<ItemStack> artifact) {
+	private static void craftRecipe$decrement(ItemStack instance, int amount, Operation<Void> original, DynamicRegistryManager registryManager, @Nullable RecipeEntry<?> recipe, DefaultedList<ItemStack> slots, int count) {
 		if (instance.isOf(DeerItems.SOLID_GOLD_DEER_ARTIFACT)) {
-			artifact.set(DeerItems.RAW_DEER.getDefaultStack());
+			slots.set(0, DeerItems.RAW_DEER.getDefaultStack());
 		} else {
 			original.call(instance, amount);
 		}
