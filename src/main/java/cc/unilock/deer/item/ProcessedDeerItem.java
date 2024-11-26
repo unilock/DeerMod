0c3170354df7c2d40b617566cd242eb536b23394
unilock <unilock@fennet.rentals>
2024-11-25 19:15:36 -0500

    processed deer sound logic update
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/deer/item/ProcessedDeerItem.java
+++ src/main/java/cc/unilock/deer/item/ProcessedDeerItem.java
@@ -1,6 +1,7 @@
 package cc.unilock.deer.item;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
@@ -18,12 +19,12 @@ public class ProcessedDeerItem extends Item {
 
 	@Override
 	public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-		if (!world.isClient) {
+		if (!world.isClient && entity instanceof LivingEntity living && living.isHolding(this)) {
 			Optional<RegistryEntry.Reference<SoundEvent>> optional = Registries.SOUND_EVENT.getRandom(world.random);
 			if (optional.isPresent()) {
 				RegistryEntry.Reference<SoundEvent> reference = optional.get();
 				if (!reference.registryKey().getValue().getPath().startsWith("music")) {
-					world.playSound(null, entity.getX(), entity.getY(), entity.getZ(), reference, SoundCategory.MASTER, 1.0F, 1.0F, world.random.nextLong());
+					world.playSound(null, living.getX(), living.getY(), living.getZ(), reference, SoundCategory.MASTER, 1.0F, ((float) world.random.nextBetween(5, 15)) / 10, world.random.nextLong());
 				}
 			}
 		}
