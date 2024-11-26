81e1a4371a5690cc684510497464a69c102a695f
unilock <unilock@fennet.rentals>
2024-11-26 15:57:08 -0500

    fancier tooltip
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/deer/item/SolidGoldDeerArtifactItem.java
+++ src/main/java/cc/unilock/deer/item/SolidGoldDeerArtifactItem.java
@@ -1,5 +1,6 @@
 package cc.unilock.deer.item;
 
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -12,10 +13,9 @@ import net.minecraft.world.World;
 import java.util.List;
 
 public class SolidGoldDeerArtifactItem extends Item {
-	// TODO: Text.translatable("item.deer.solid_gold_deer_artifact.tooltip")
-	private final String tooltipString = "Every deer is gold";
-	private final int tooltipLength = tooltipString.length();
-	private final int maxTickOffset = ((tooltipLength - 1) * 5) + 4;
+	private String tooltipString;
+	private int tooltipLength;
+	private int maxTickOffset;
 	private int tickOffset = 0;
 
 	public SolidGoldDeerArtifactItem(Settings settings) {
@@ -25,14 +25,27 @@ public class SolidGoldDeerArtifactItem extends Item {
 	@Override
 	public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
 		super.inventoryTick(stack, world, entity, slot, selected);
-		tickOffset++;
-		if (tickOffset > maxTickOffset) tickOffset = 0;
+		if (world.isClient && tooltipString != null) {
+			tickOffset++;
+			if (tickOffset > maxTickOffset) tickOffset = 0;
+		}
 	}
 
 	@Override
 	public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
 		super.appendTooltip(stack, context, tooltip, type);
-		int offset = MathHelper.floor((double) tickOffset / 5);
+
+		if (tooltipString == null) {
+			try {
+				tooltipString = I18n.translate("item.deer.solid_gold_deer_artifact.tooltip")+" ";
+			} catch (Throwable ignored) {
+				tooltipString = "Every deer is gold";
+			}
+			tooltipLength = tooltipString.length();
+			maxTickOffset = ((tooltipLength - 1) * 3) + 2;
+		}
+
+		int offset = MathHelper.floor((double) tickOffset / 3);
 		String first = tooltipString.substring(0, offset);
 		String second = tooltipString.substring(offset, offset + 1);
 		String third = tooltipString.substring(offset + 1, tooltipLength);
