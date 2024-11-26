458f02196baad3f9ac0de2318b623b330661924b
unilock <unilock@fennet.rentals>
2024-11-26 08:59:59 -0500

    sinytra connector compat
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/cc/unilock/deer/DeerPlugin.java
@@ -0,0 +1,51 @@
+package cc.unilock.deer;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class DeerPlugin implements IMixinConfigPlugin {
+	private static final boolean CONNECTOR = FabricLoader.getInstance().isModLoaded("connector");
+
+	@Override
+	public void onLoad(String mixinPackage) {
+		// NO-OP
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		if (mixinClassName.endsWith("CookingRecipeSerializerMixin")) {
+			return !CONNECTOR;
+		}
+		return true;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+		// NO-OP
+	}
+
+	@Override
+	public List<String> getMixins() {
+		return null;
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+		// NO-OP
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+		// NO-OP
+	}
+}
